f01e1dfb9d957b3ea843fce425aea0cc2214b0a5
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-03-10 10:11:48 -0300

    feat: add page scroll native css

---
+++ page-scroll-snap-native/src/pages/index.js
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <section>
+        <h1>Page one</h1>
+      </section>
+      <section>
+        <h1>Page two</h1>
+      </section>
+      <section>
+        <h1>Page three</h1>
+      </section>
+      <section>
+        <h1>Page four</h1>
+      </section>
+      <section>
+        <h1>Page 5</h1>
+      </section>
+    </div>
+  );
+}
